d90bf3312196027f211a22128b46dd7bd1995180
Animoh-co <81622182+Animoh-co@users.noreply.github.com>
2021-10-12 13:51:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload(){
+
+}
+
+
+function setup(){
+Canvas1 = createCanvas(450,450);
+Canvas1.center();
+}
+
+
+function draw(){
+
+}
+
+function takeimage(){
+    save("myimage.png");
+}
